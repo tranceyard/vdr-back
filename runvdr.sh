57a0c49f95997c4d772daff1434829b5bc514919
tnds82 <tiagondasilva@gmail.com>
2020-02-22 14:32:00 +0000

    Update runvdr.sh

--- runvdr.sh
+++ runvdr.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-/usr/bin/vdr --chartab=ISO-8859-9 --port=6419 -P svdrposd -P svdrpservice -P dummydevice -P epgsearch -P streamdev-server -P xvdr -P vnsiserver -P live -P 'epgsearch --config=/etc/vdr/plugins/epgsearch' -P ddci2 -v /srv/vdr/video
+/usr/bin/vdr --chartab=ISO-8859-9 --port=6419 -P svdrposd -P svdrpservice -P dummydevice -P epgsearch -P streamdev-server -P vnsiserver -P live -P 'epgsearch --config=/etc/vdr/plugins/epgsearch' -P ddci2 -v /srv/vdr/video
 
