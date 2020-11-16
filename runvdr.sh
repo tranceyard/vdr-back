ed4d2e209f975bc1b73492e7bc8c03760388566f
tranceyard <47573138+tranceyard@users.noreply.github.com>
2020-11-16 05:06:57 +0000

    Update runvdr.sh

--- runvdr.sh
+++ runvdr.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
 
-/usr/bin/vdr --chartab=ISO-8859-1 --port=6419 -P svdrposd -P svdrpservice -P dummydevice -P epgsearch -P streamdev-server -P vnsiserver -P 'live -i 0.0.0.0 -p 8008' -P 'epgsearch --config=/etc/vdr/plugins/epgsearch' -P ddci2
-
+/usr/bin/vdr --chartab=ISO-8859-9 --port=6419 -P svdrposd -P svdrpservice -P dummydevice -P epgsearch -P streamdev-server -P xvdr -P vnsiserver -P live -P 'epgsearch --config=/etc/vdr/plugins/epgsearch' -P ddci2 -v /srv/vdr/video
